e270ba204a84f3f506cef367f9f9e82476f47205
rachel-o-donnell <hellotherachelo@gmail.com>
2023-04-18 17:53:39 +0000

    Customised django admin

--- items/admin.py
+++ items/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Item, Category
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Category)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'dimensions',
+        'image',
+        'how_many_in_stock'
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
--- items/models.py
+++ items/models.py
@@ -6,6 +6,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=100, null=True, blank=True)
 
