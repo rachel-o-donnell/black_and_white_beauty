d9b1a520e59b7a48781a14366ce40df995b8c4f6
rachel-o-donnell <hellotherachelo@gmail.com>
2023-04-18 16:36:14 +0000

    Add items app, models, admin and migrations

---
+++ items/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Item(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=1024)
+    dimensions = models.CharField(max_length=250)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    estimated_dispatch = models.DateField(null=True, blank=True)
+    in_stock = models.BooleanField(null=True, blank=True)
+    how_many_in_stock = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
