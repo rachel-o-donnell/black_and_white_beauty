6d964c0a38c09e2c8a91c438e29fd2a583098a58
rachel-o-donnell <hellotherachelo@gmail.com>
2023-04-19 17:08:05 +0000

    Add searchbar functionality

--- items/views.py
+++ items/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Item
+
+from django.db.models import Q
 
 # Create your views here.
 
@@ -8,6 +11,24 @@ def all_items(request):
     """ Shows all items, including sorting and search queries """
 
     items = Item.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You must enter a search word")
+                return redirect(reverse('items'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            items = items.filter(queries)
+
+    context = {
+        'items': items,
+        'search_term': query,
+    }
+
+    return render(request, 'items/items.html', context)
 
     context = {
         'items': items,
