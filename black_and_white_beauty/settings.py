fbe14e113b5aff46ff4749de9566fd1833672197
rachel-o-donnell <hellotherachelo@gmail.com>
2023-04-22 21:30:17 +0000

    Add webhook handler, urls, views js and secret

--- black_and_white_beauty/settings.py
+++ black_and_white_beauty/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['black-and-white-beauty.herokuapp.com', 'localhost']
@@ -190,5 +189,4 @@ STANDARD_DELIVERY_PERCENTAGE = 15
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
