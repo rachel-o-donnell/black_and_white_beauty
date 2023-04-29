91e55aaa184d294201b9060235d28280971bf759
rachel-o-donnell <hellotherachelo@gmail.com>
2023-04-29 15:26:48 +0000

    Add ItemForm to Items app for admin access

---
+++ items/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Item, Category
+
+
+class ItemForm(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
