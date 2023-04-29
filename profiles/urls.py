21d6f8a6865a283e0a2876f246bd7c507bbe7f93
rachel-o-donnell <hellotherachelo@gmail.com>
2023-04-29 11:37:06 +0000

    Add past orders to profile - views, template urls
    And basic styling

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('past_orders/<order_number>', views.past_orders, name='past_orders')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 def profile(request):
@@ -18,11 +20,30 @@ def profile(request):
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
+    print("ORDERS: ", orders, Order.objects.filter(user_profile=profile))
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def past_orders(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is an order confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
