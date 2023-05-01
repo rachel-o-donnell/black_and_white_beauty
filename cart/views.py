8a704e8c7e5f1ffdc9c2360f55bccaa8ecf603e2
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-01 22:21:19 +0000

    Resolve #55 Fix update cart with defensive logic
    Prevent users from adding more than the limit of 99 or the same item
    items or adding negative numbers

--- cart/views.py
+++ cart/views.py
@@ -23,7 +23,8 @@ def add_to_cart(request, item_id):
     else:
         cart[item_id] = quantity
 
-    messages.success(request, f'You added {quantity} x "{item.name}" to your cart.')
+    messages.success(
+        request, f'You added {quantity} x "{item.name}" to your cart.')
     request.session['cart'] = cart
     return redirect(redirect_url)
 
@@ -35,12 +36,38 @@ def adjust_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[item_id] = quantity
-        messages.success(request, f'Cart updated. You now have {quantity} x "{item.name}" in your cart.')
+    """ Prevent anyone ordering more than the limit of 99 items or entering negative values"""
+    if quantity > 99:
+        messages.error(
+            request,
+            f"You cannot have more than 99 { item.name }'s in your cart."
+        )
+
+    elif quantity < 0:
+        messages.error(
+            request,
+            "You cannot add negative quantities. Please remove the item using the 'Remove' button or update the number of items."
+        )
+
+    elif quantity > 0:
+        if cart[item_id] + quantity > 99:
+            messages.error(
+                request,
+                f"You cannot have more than 99 { item.name }'s in your cart."
+            )
+        elif cart[item_id] + quantity < 0:
+            messages.error(
+                request,
+                f"You cannot add negative quantities. Please remove the item using the 'Remove' button or update the number of items.",
+            )
+        else:
+            cart[item_id] = quantity
+            messages.success(
+                request, f'Cart updated. You now have {quantity} x "{item.name}" in your cart.')
     else:
         cart.pop(item_id)
-        messages.success(request, f'You added {quantity} x "{item.name}" to your cart')
+        messages.success(
+            request, f'You added {quantity} x "{item.name}" to your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -59,5 +86,6 @@ def remove_from_cart(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'An error occured removing "{e}" from your cart')
+        messages.error(
+            request, f'An error occured removing "{e}" from your cart')
         return HttpResponse(status=500)
