a6206beb8f51a3c55f7c9ecea352e242524218bb
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-02 09:36:15 +0000

    Add commissions model and migrate

--- commissions/models.py
+++ commissions/models.py
@@ -1,1 +1,15 @@
+from django.db import models
 
+
+class Commissions(models.Model):
+    category = models.ForeignKey(
+        'items.Category', null=True, blank=True, on_delete=models.SET_NULL)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    name = models.CharField(max_length=254)
+    brief = models.CharField(max_length=1024, null=True, blank=True)
+    description = models.TextField(max_length=1024)
+    dimensions = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
