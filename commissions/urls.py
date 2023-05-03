45cd3c06c8bce17962058ba0386927e3b81188c9
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-03 12:08:35 +0000

    Revert "Resolve #58: Bug - NoReverseMatch 'commissions'"
    This reverts commit 0c0f5b0e34526709ba89d40afec15d3b531c3e5f.

--- commissions/urls.py
+++ commissions/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_commissions, name='commissions'),
-    path('<int:commission_id>/', views.commission_detail, name='commission_detail'),
+    path('', views.all_commissions, name='all_commissions'),
 ]
--- commissions/views.py
+++ commissions/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from django.db.models.functions import Lower
 
-from items.models import Item, Category
 from .models import Commissions
 
 
@@ -12,40 +10,20 @@ def all_commissions(request):
 
     commissions = Commissions.objects.all()
     query = None
-    categories = None
 
-    if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            commissions = commissions.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+    if 'q' in request.GET:
 
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You must enter a search word")
-                return redirect(reverse('commissions'))
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You must enter a search word")
+            return redirect(reverse('commissions'))
 
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
-            commissions = commissions.filter(queries)
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        commissions = commissions.filter(queries)
 
     context = {
         'commissions': commissions,
         'search_term': query,
-        'current_categories': categories,
     }
 
     return render(request, 'commissions/commissions.html', context)
-
-
-def commission_detail(request, commission_id):
-    """ Shows individual commission details """
-
-    commission = get_object_or_404(Commissions, pk=commission_id)
-
-    context = {
-        'commission': commission,
-    }
-
-    return render(request, 'commissions/commission_detail.html', context)
