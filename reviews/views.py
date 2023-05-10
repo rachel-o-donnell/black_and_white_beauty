4ed4238fb0360274f78088cb07603b474e254eff
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-10 16:54:49 +0000

    Add add_review views, templates and widgets
    add_review.html, reviews.html, and custom widget/template

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from items.models import Item
+from django.contrib import messages
 from .models import Review
+from .forms import ReviewForm
 from profiles.models import UserProfile
 
 
@@ -13,6 +15,46 @@ def reviews(request, item_id):
 
     context = {
         'reviews': reviews,
+        'item': item,
+    }
+
+    return render(request, template, context)
+
+
+def add_review(request, item_id):
+    """
+    Allow users to add a review
+    """
+    if not request.user.is_authenticated:
+        messages.error(request,
+                       'You need to be logged in to add a review.')
+        return redirect(reverse('account_login'))
+
+    user = UserProfile.objects.get(user=request.user)
+    item = Item.objects.get(id=item_id)
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            review = form.save()
+            review.item = item
+            review.user = request.user
+            review.save()
+            messages.success(
+                request, f"Your review for '{item.name}' has been submitted!")
+            return redirect(reverse('item_detail', args=[item.id]))
+        else:
+            messages.error(
+                request, "Sorry, something went wrong. Please ensure all fields are valid")
+            return redirect(reverse('item_detail', args=[item.id]))
+    else:
+        form = ReviewForm()
+
+    template = 'reviews/add_review.html'
+
+    context = {
+        'form': form,
         'item': item,
     }
 
