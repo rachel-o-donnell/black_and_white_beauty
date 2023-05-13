668fe994e2c81bdac7004c0ee18b9138006e8a9b
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-13 11:09:49 +0000

    Remove unneeded localhost

--- black_and_white_beauty/settings.py
+++ black_and_white_beauty/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['black-and-white-beauty.herokuapp.com', 'localhost',
-                 '8000-rachel-o-donnell-black-a-9mqrixu1vk.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['black-and-white-beauty.herokuapp.com', 'localhost']
 
 
 # Application definition
