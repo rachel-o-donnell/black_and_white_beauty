9988813e6a836505280137ede3e45c581eb23e6c
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-19 09:01:18 +0000

    Ammend profiles python files after validation

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
