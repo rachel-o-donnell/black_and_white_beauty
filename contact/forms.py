7d27f922ed370eaa765fff0ae1a6866fc74b8ab2
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-26 20:40:04 +0000

    Final changes to contact form after validation

--- contact/forms.py
+++ contact/forms.py
@@ -9,34 +9,28 @@ class ContactForm(forms.ModelForm):
     """
     class Meta:
         model = Contact
-        fields = '__all__'
 
-    image = forms.ImageField(
-        label='Add a pic below if its relevent to your query', required=False,
-        widget=CustomClearableFileInput)
+        exclude = ('image_url',)
+
+    image = forms.ImageField(required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and remove auto-generated
-        labels
-        """
+        """Add placeholders and remove auto-generated labels """
         super().__init__(*args, **kwargs)
 
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
-            'enquiry_subject': 'Reason for contacting',
             'body': 'We welcome your enquiry!'
                     ' Please attach a photo below if relevent',
-            'image_url': 'Current image',
-            'image': 'If relevent',
             'order_number': 'If relevent',
             'dimensions': 'If relevent',
         }
 
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'image' and field != 'enquiry_subject':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
