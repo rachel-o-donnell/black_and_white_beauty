197a7a893d79338162a9652008aecd4392f03437
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-29 17:01:46 +0000

    RESOLVE #91 Add styling and refactor reviews app
    Fix filename not showing when uploading image
     - Changed filename id's to classes and ammended corresponding Javascript

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Review
 
 from django.contrib.auth.models import User
@@ -11,26 +12,19 @@ class ReviewForm(forms.ModelForm):
         model = Review
         exclude = ('user', 'image_url')
 
-    image = forms.ImageField(
-        label='We love seeing how you have styled your pieces'
-        ' in your home', required=False)
+    image = forms.ImageField(required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         placeholders = {
-            'item': 'Item name',
-            'commission': 'Commission piece',
             'title': 'One word description',
             'body': 'Go on, give us some more detail...',
-            'image': 'Image',
-            'featured': "Please tick this if you would like your review "
-                        " to be published on the site"
-
         }
 
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'image' and field != 'featured' and field != 'item' and field != 'commission':  # noqa
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
