a58c1fde43bebbe6b615c0b1b210fb3c5f5d0eff
rachel-o-donnell <hellotherachelo@gmail.com>
2023-05-31 16:34:46 +0000

    Fix JAvascript files after validation

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -122,5 +122,5 @@ form.addEventListener('submit', function (ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages	
         location.reload();
-    })
+    });
 });
